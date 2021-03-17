981ef79cb5f972eae0992aa8cb27299924ca2076
vaabe <accounts@vinzaabe.com>
2021-03-17 17:31:22 -0400

    update fig orna

--- adcmaps.jl
+++ adcmaps.jl
@@ -8,11 +8,24 @@ FAmap = readdlm("./fitdata/FA.csv", ',')
 
 #setcolormap(8)
 
+gr(grid = false, size = (900, 900))
+
 heatmap(MDmap, 
 		aspect_ratio = 1,
-		title = "Mean Diffusivity (MD)") 
+		xlim = (0, 96), 
+		ylim = (0, 96), 
+		xlabel = "x", 
+		ylabel = "y",
+		title = "Mean Diffusivity (MD)", 
+		) 
 savefig("./pngs/MDmap.png")
-#
-#heatmap(FAmap)
-#savefig("./pngs/FAmap.png")
-#
+
+heatmap(FAmap, 
+		aspect_ratio = 1,
+		xlim = (0, 96), 
+		ylim = (0, 96), 
+		xlabel = "x", 
+		ylabel = "y",
+		title = "Fractional Anisotropy (FA)",
+		) 
+savefig("./pngs/FAmap.png")
